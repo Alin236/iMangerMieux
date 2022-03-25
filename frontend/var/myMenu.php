c2a5cfb34d862582deaa129bb4aefb2315f61e69
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-03-25 11:43:02 +0100

    Détail esthétique

--- frontend/var/myMenu.php
+++ frontend/var/myMenu.php
@@ -1,8 +1,8 @@
 <?php
-// un tableau qui définit la structure du site
-$myMenu = array(
-    // idPage titre
-    'accueil' => array( 'Accueil' ),
-    'connection' => array( 'Connexion' ),
-);
+    //Un tableau qui définit la structure du site
+    $myMenu = array(
+        //idPage => titre
+        'accueil' => array( 'Accueil' ),
+        'connection' => array( 'Connexion' ),
+    );
 ?>
