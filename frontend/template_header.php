beebe62cbc8094ecfac2fc50892844f8c5f6e094
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-03-25 11:57:57 +0100

    Changement du header pour la connexion

--- frontend/template_header.php
+++ frontend/template_header.php
@@ -1,11 +1,19 @@
 <?php
-    echo '<!DOCTYPE HTML>
-    <html lang="fr">
+    session_start();
+    $connection;
+    if(isset($_SESSION['login'])){
+        $connection = '<p>Bienvenue '.$_SESSION['login'].'</p>';
+    }
+    else{
+        $connection = '<a href="http://imangermieux/frontend/index.php?page=connection">Connexion</a>';
+    }
+    echo "<!DOCTYPE HTML>
+    <html lang=\"fr\">
     <head> 
         <title>iMangerMieux</title> 
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-        <link href="tp1.css" rel="stylesheet" type="text/css">
+        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>
+        <link href=\"tp1.css\" rel=\"stylesheet\" type=\"text/css\">
     </head>
     <body>
-        <header><h1>iMangerMieux</h1></header>';
+        <header><h1>iMangerMieux</h1>$connection</header>";
 ?>
