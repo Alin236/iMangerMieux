ab5480bc2ebd51cc702b422832a2dfd41b8b33cf
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-03-25 10:37:29 +0100

    Correction de la base du projet

--- frontend/template_header.php
+++ frontend/template_header.php
@@ -1,13 +1,11 @@
 <?php
-    function myHeader($currentLeng){
-        echo "<!DOCTYPE HTML>
-        <html lang=\"$currentLeng\">
-        <head> 
-            <title>iMangerMieux</title> 
-            <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>
-            <link href=\"tp1.css\" rel=\"stylesheet\" type=\"text/css\">
-        </head>
-        <body>
-            <header><h1>iMangerMieux</h1></header>";
-    }
+    echo '<!DOCTYPE HTML>
+    <html lang="fr">
+    <head> 
+        <title>iMangerMieux</title> 
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <link href="tp1.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <header><h1>iMangerMieux</h1></header>';
 ?>
