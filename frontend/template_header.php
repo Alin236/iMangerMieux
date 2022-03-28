47b385f74c9afbab265fe63f2ee8a3879890e653
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-03-28 14:36:25 +0200

    Importation du css du tp2 (css temporaire)

--- frontend/template_header.php
+++ frontend/template_header.php
@@ -12,7 +12,7 @@
         <head> 
             <title>iMangerMieux</title> 
             <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>
-            <link href=\"tp1.css\" rel=\"stylesheet\" type=\"text/css\">
+            <link href=\"css/tp1.css\" rel=\"stylesheet\" type=\"text/css\">
             <script src=\"https://code.jquery.com/jquery-3.5.1.min.js\"></script>
             <script src=\"https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js\"></script>
         </head>
