3759cd51a65d57038d4261b1f3a8e430f55e5120
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-03-28 16:39:53 +0200

    Arret d'utilisation du css tp1, début d'utilisation de bootstrap

--- frontend/template_header.php
+++ frontend/template_header.php
@@ -7,15 +7,15 @@
     else{
         $connection = '<a href="http://imangermieux/frontend/index.php?page=connection">Connexion</a>';
     }
-    echo "<!DOCTYPE HTML>
-        <html lang=\"fr\">
-        <head> 
-            <title>iMangerMieux</title> 
-            <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>
-            <link href=\"css/tp1.css\" rel=\"stylesheet\" type=\"text/css\">
-            <script src=\"https://code.jquery.com/jquery-3.5.1.min.js\"></script>
-            <script src=\"https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js\"></script>
-        </head>
-        <body>
-            <header><h1>iMangerMieux</h1>$connection</header>";
 ?>
+<!DOCTYPE HTML>
+<html lang="fr">
+<head> 
+    <title>iMangerMieux</title> 
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+</head>
+<body>
+    <header><h1>iMangerMieux</h1><?php echo $connection ?></header>
