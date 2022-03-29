52ee46096ad6c554d7d1be9b7d9985d6a4d1c94a
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-03-29 09:27:03 +0200

    Remise du bootstrap

--- frontend/template_header.php
+++ frontend/template_header.php
@@ -7,18 +7,18 @@
     else{
         $connection = '<a href="http://imangermieux/frontend/index.php?page=connection">Connexion</a>';
     }
-    echo "<!DOCTYPE HTML>
-        <html lang=\"fr\">
-        <head> 
-            <title>iMangerMieux</title> 
-            <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>
-            <link href=\"tp1.css\" rel=\"stylesheet\" type=\"text/css\">
-            <script src=\"https://code.jquery.com/jquery-3.5.1.min.js\"></script>
-            <script src=\"https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js\"></script>
-            <script src=\"https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js\"></script>
-            <script src=\"https://editor.datatables.net/extensions/Editor/js/dataTables.editor.min.js\"></script>
-            <script src=\"https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js\"></script>
-        </head>
-        <body>
-            <header><h1>iMangerMieux</h1>$connection</header>";
 ?>
+<!DOCTYPE HTML>
+<html lang="fr">
+<head> 
+    <title>iMangerMieux</title> 
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+    <script src="https://editor.datatables.net/extensions/Editor/js/dataTables.editor.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+</head>
+<body>
+    <header><h1>iMangerMieux</h1>$connection</header>
