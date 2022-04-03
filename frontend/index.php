bfba7129e33127464bc8caa0f7b804ddcccad3ee
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-04-03 21:40:50 +0200

    Modification de la structure

--- frontend/index.php
+++ frontend/index.php
@@ -1,33 +1,27 @@
 <?php
     require_once('template_header.php');
-?>
-    <div class="container">
-        <div class="columnMenu">
-            <?php
-                require_once('template_menu.php');
+    require_once('template_menu.php');
+    $currentPageId = 'accueil';
+    if(isset($_SESSION['id_utilisateur'])){
+        if(isset($_GET['page'])){
+            $currentPageId = $_GET['page'];
+            if($currentPageId == 'index')
                 $currentPageId = 'accueil';
-                if(isset($_SESSION['id_utilisateur'])){
-                    if(isset($_GET['page'])){
-                        $currentPageId = $_GET['page'];
-                        if($currentPageId == 'index')
-                            $currentPageId = 'accueil';
-                    }
-                }
-                else{
-                    $currentPageId = 'authenticate';
-                }
-                renderMenuToHTML($currentPageId);
-            ?>
-        </div>
-        </div>
-        <section class="container-fluid">
-            <?php
-                $pageToInclude = "$currentPageId.php";
-                if(is_readable($pageToInclude))
-                    require_once($pageToInclude);
-                else
-                    require_once("error.php");
-            ?>
-        </section>
+        }
+    }
+    else{
+        $currentPageId = 'authenticate';
+    }
+    renderMenuToHTML($currentPageId);
+?>
+    <section class="container-fluid">
+        <?php
+            $pageToInclude = "$currentPageId.php";
+            if(is_readable($pageToInclude))
+                require_once($pageToInclude);
+            else
+                require_once("error.php");
+        ?>
+    </section>
     
 <?php require_once('template_footer.php')?>
