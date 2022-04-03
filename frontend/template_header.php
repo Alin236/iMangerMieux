d6da84ac5ab38bf6b76e8dd5c89230dbd2f85783
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-04-03 21:41:22 +0200

    Supression mineure d'espace

--- frontend/template_header.php
+++ frontend/template_header.php
@@ -10,15 +10,12 @@
 ?>
 <!DOCTYPE HTML>
 <html lang="fr">
-<head> 
+<head>
     <title>iMangerMieux</title> 
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
-   
-  
-    
 </head>
 <body>
     <header><h1>iMangerMieux</h1><?php echo $textConnection ?></header>
