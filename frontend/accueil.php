e9cb56ff39ad31f88412dfc63de429382f8753a9
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-04-04 02:08:55 +0200

    Frontend : amélioration du graph des 5 fruits et légumes

--- frontend/accueil.php
+++ frontend/accueil.php
@@ -1,12 +1,15 @@
 <h2>Accueil</h2>
 <p id='id_utilisateur' class='d-none'><?php echo $_SESSION['id_utilisateur'] ?></p>
-<p id='jour' class='d-none'>7</p>
 <div class="card-deck">
     <div class="card">
         <div class="card-header">
             <h3 class="card-title text-center">Moyenne de fruits et légumes</h3>
         </div>
         <div id="countFruitVegetable" class="card-body">
+        </div>
+        <div class="card-footer">
+            <label for='jour'>Jours :</label>
+            <input id='jour' value='7'>
         </div>
     </div>
 </div>
--- frontend/js/graph.js
+++ frontend/js/graph.js
@@ -1,15 +1,19 @@
 var id_utilisateur;
-var jour;
 
 $(document).ready(function() {
     id_utilisateur = $('#id_utilisateur').html();
-    jour = $('#jour').html();
+    plotCountFruitVegetable();
+});
+
+$( "#jour" ).change(function() {
     plotCountFruitVegetable();
 });
 
 function plotCountFruitVegetable(){
+    const jour = $('#jour').val();
     const url = '/backend/countFruitVegetable.php?id_utilisateur=' + id_utilisateur + '&jour=' + jour;
     $.get(url, function(response){
+        $('#countFruitVegetable').empty();
         $('#countFruitVegetable').append('<p class="card-text text-center">' + response + '/5</p>');
     }, 'json').fail(function(){
         console.log('Fail : chargement de la consommation de fruits et légumes');
