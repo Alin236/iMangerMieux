4ab0238b1fe929b9ec058c897cce4533e24dcaa9
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-04-04 05:30:57 +0200

    Frontend : modification de l'esthétique

--- frontend/accueil.php
+++ frontend/accueil.php
@@ -1,22 +1,26 @@
-<h2>Accueil</h2>
+<h2 class="text-center">Accueil</h2>
 <p id='id_utilisateur' class='d-none'><?php echo $_SESSION['id_utilisateur'] ?></p>
-<div class="card-deck">
-    <div class="card">
-        <div class="card-header">
-            <h3 class="card-title text-center">Moyenne de fruits et légumes</h3>
-        </div>
-        <div id="countFruitVegetable" class="card-body">
-        </div>
-        <div class="card-footer">
-            <label for='jour'>Jours :</label>
-            <input id='jour' value='7'>
+<div class="row">
+    <div class="col-2">
+        <div class="card h-100 shadow">
+            <div class="card-header">
+                <h3 class="card-title text-center">Moyenne de fruits et légumes</h3>
+            </div>
+            <div id="countFruitVegetable" class="card-body">
+            </div>
+            <div class="card-footer">
+                <label for='jour'>Jours :</label>
+                <input id='jour' value='7'>
+            </div>
         </div>
     </div>
-    <div class="card">
-        <div class="card-header">
-            <h3 class="card-title text-center">Analyse journalière</h3>
-        </div>
-        <div id="dailyAnalysis" class="card-body d-flex">
+    <div class="col">
+        <div class="card h-100 shadow">
+            <div class="card-header">
+                <h3 class="card-title text-center">Analyse journalière</h3>
+            </div>
+            <div id="dailyAnalysis" class="card-body d-flex justify-content-center">
+            </div>
         </div>
     </div>
 </div>
--- frontend/js/graph.js
+++ frontend/js/graph.js
@@ -15,7 +15,7 @@ function plotCountFruitVegetable(){
     const url = '/backend/countFruitVegetable.php?id_utilisateur=' + id_utilisateur + '&jour=' + jour;
     $.get(url, function(response){
         $('#countFruitVegetable').empty();
-        $('#countFruitVegetable').append('<p class="card-text text-center">' + response + '/5</p>');
+        $('#countFruitVegetable').append('<p class="card-text text-center fs-1">' + response + '/5</p>');
     }, 'json').fail(function(){
         console.log('Fail : chargement de la consommation de fruits et légumes');
     });
