684e6bc76e1e0639d00cff146bcd7741dc1a4530
Alin236 <nilavan.deva@etu.imt-lille-douai.fr>
2022-04-04 08:06:06 +0200

    Frontend : correction d'importation

--- frontend/template_header.php
+++ frontend/template_header.php
@@ -15,7 +15,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    
+    <script src="https://d3js.org/d3.v7.min.js"></script>
 </head>
 <body>
     <header class="bg-light">
